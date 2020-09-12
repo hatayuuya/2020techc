1695e5f3778a6ae83c3f8f69d6ae7c9e694d9b48
hataya <18d2030010hy@edu.tech.ac.jp>
2020-09-12 08:27:17 +0000

    text.sql

--- etc/mysql/text.sql
+++ etc/mysql/text.sql
@@ -4,3 +4,10 @@ CREATE TABLE `users` (
     `password` TEXT NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE `bbs_entries` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` TEXT NOT NULL,
+    `body` TEXT NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+);
